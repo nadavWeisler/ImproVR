323fd9c9d24d5107aff4b631e186d9fc61397e6b
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2021-07-04 14:38:04 +0300

    Continue 4/7

--- improVR/Assets/Scripts/AudioManager.cs
+++ improVR/Assets/Scripts/AudioManager.cs
@@ -2,19 +2,131 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour
- {
-     public AudioSource audioSource;
-     Queue<AudioClip> clipQueue;
-     void Update()
-     {
-         if (audioSource.isPlaying == false && clipQueue.Count > 0) {
-             audioSource.clip = clipQueue.Dequeue();
-             audioSource.Play();
-         }
-     }
-     public void PlaySound(AudioClip clip)
-     {
-         clipQueue.Enqueue(clip);
-     }
- }
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject musicBox1;
+    [SerializeField]
+    private GameObject musicBox2;
+    [SerializeField]
+    private GameObject musicBox3;
+    [SerializeField]
+    private GameObject musicBox4;
+    [SerializeField]
+    private GameObject musicBox5;
+    [SerializeField]
+    private GameObject musicBox6;
+    [SerializeField]
+    private GameObject musicBox7;
+    [SerializeField]
+    private GameObject musicBox8;
+    [SerializeField]
+    private GameObject musicBox9;
+    [SerializeField]
+    private GameObject musicBox10;
+    [SerializeField]
+    private GameObject musicBox11;
+    [SerializeField]
+    private GameObject musicBox12;
+
+    [SerializeField]
+    private AudioClip bush;
+    [SerializeField]
+    private AudioClip fire;
+    [SerializeField]
+    private AudioClip firefly;
+    [SerializeField]
+    private AudioClip flower;
+    [SerializeField]
+    private AudioClip foxGreg;
+    [SerializeField]
+    private AudioClip otterLarry;
+    [SerializeField]
+    private AudioClip owl;
+    [SerializeField]
+    private AudioClip frog;
+    [SerializeField]
+    private AudioClip snake;
+    [SerializeField]
+    private AudioClip stone1;
+    [SerializeField]
+    private AudioClip stone2;
+    [SerializeField]
+    private AudioClip stone3;
+    [SerializeField]
+    private AudioClip tree1;
+    [SerializeField]
+    private AudioClip tree2;
+    [SerializeField]
+    private AudioClip tree3;
+    private List<AudioSource> loop;
+    private Dictionary<string, AudioClip> dict;
+    private float speed;
+    private bool isPlaying;
+
+    [SerializeField]
+    private GameObject needle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 12; i++)
+        {
+            loop[i] = gameObject.AddComponent<AudioSource>();
+            createDic();
+        }
+        this.speed = 1f;
+        this.isPlaying = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isPlaying)
+        {
+            this.needle.transform.RotateAround(this.transform.position, new Vector3(0f, 1f, 0f), 30f * Time.deltaTime * this.speed);
+        }
+    }
+    private void playLoop()
+    {
+        for (int i = 0; i < 12; i++)
+        {
+            loop[i].Play();
+            while (loop[i].isPlaying)
+            {
+                continue;
+            }
+            i++;
+        }
+    }
+    private void createDic()
+    {
+        dict.Add("bush", this.bush);
+        dict.Add("fire", this.fire);
+        dict.Add("firefly", this.firefly);
+        dict.Add("flower", this.flower);
+        dict.Add("foxGreg", this.foxGreg);
+        dict.Add("otterLarry", this.otterLarry);
+        dict.Add("owl", this.owl);
+        dict.Add("frog", this.frog);
+        dict.Add("snake", this.snake);
+        dict.Add("stone1", this.stone1);
+        dict.Add("stone2", this.stone2);
+        dict.Add("stone3", this.stone3);
+        dict.Add("tree1", this.tree1);
+        dict.Add("tree2", this.tree2);
+        dict.Add("tree3", this.tree3);
+    }
+
+    public AudioClip getAudioClip(string name)
+    {
+        return dict[name];
+    }
+
+    private void muteEnvironment(bool mute)
+    {
+        GameObject[] inv = GameObject.FindGameObjectsWithTag("music");
+        for (int i = 0; i < inv.Length; i++)
+        {
+            inv[i].GetComponent<AudioObject>().muteObject(mute); 
+        }
+    }
+}
---
+++ improVR/Assets/Scripts/AudioObject.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AudioObject : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private bool mute;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.audioSource = this.GetComponent<AudioSource>();
+        this.audioSource.loop = true;
+        this.mute = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mute)
+        {
+            this.audioSource.mute = true;
+            this.audioSource.loop = false;
+        }
+        else
+        {
+            this.audioSource.mute = false;
+            this.audioSource.loop = true;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.transform.tag == "hand")
+        {
+            GameObject small_clone = Instantiate(gameObject, collision.transform.position, gameObject.transform.rotation);
+            Vector3 scale = small_clone.transform.localScale;
+            small_clone.transform.localScale = new Vector3(scale.x / 10f, scale.y / 10f, scale.z / 10f);
+            small_clone.name = gameObject.name;
+            small_clone.tag = gameObject.tag;
+        }
+    }
+
+    public void muteObject(bool mute)
+    {
+        this.mute = mute;
+    }
+}
