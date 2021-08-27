9146f127e67578760b44e0c2686e9a014a902fcf
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2021-08-27 19:24:11 +0300

    Continue

--- improVR/Assets/Scripts/Tool.cs
+++ improVR/Assets/Scripts/Tool.cs
@@ -7,19 +7,22 @@ public class Tool : MonoBehaviour
     private float x;
     private float y;
     private float z;
+    private Vector3 startPos
 
     // Start is called before the first frame update
     void Start()
     {
-        this.x = this.transform.position.x;
-        this.y = this.transform.position.y;
-        this.z = this.transform.position.z;
+        this.startPos = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(this.transform.position.y < 0.05) 
+        {
+            this.createNewObject(other.transform, new Vector3(x, y, z), Quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 
     void createNewObject(Transform parent, Vector3 position, Quaternion rotation)
@@ -27,19 +30,6 @@ public class Tool : MonoBehaviour
         var newObj = Instantiate(parent, position, rotation);
         Rigidbody newRig = newObj.transform.GetComponent<Rigidbody>();
         newRig.useGravity = true;
-        newRig.mass = 1;
-        newRig.inertiaTensor = new Vector3(1, 1, 1);
-        //newRig.isKinematic = false;
-    }
-
-
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.transform.gameObject.tag == "plane")
-        {
-            print("plane");
-            this.createNewObject(other.transform, new Vector3(x, y, z), Quaternion.identity);
-            Destroy(this.gameObject);
-        }
+        newRig.isKinematic = false;
     }
 }
